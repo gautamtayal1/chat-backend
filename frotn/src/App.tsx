f7518cbda5c499373d267081ab26db1cfbc78a1d
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-22 14:52:05 +0530

    backend socket & frontend setup

---
+++ frotn/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+ return(
+    <div className='h-screen w-screen bg-black flex flex-col items-center justify-center text-yellow-50'>
+      hello
+    </div>
+ )
+}
+
+export default App
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
+const wss = new WebSocketServer({port: 8080})
+
+interface User {
+  socket: WebSocket;
+  room: string
+}
+
+let allSockets: User [] = []
+
+wss.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    //@ts-ignore
+    const parsedMessage = JSON.parse(message) //socket will always return string even if we send an object
+
+    if(parsedMessage.type == "join"){
+       allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId
+       })
+    }  
+    if(parsedMessage.type == "chat"){
+      //@ts-ignore
+      const currentUserRoom = allSockets.find((x) =>  x.socket == socket).room
+      for(let i = 0; i < allSockets.length; i++){
+        if (allSockets[i].room == currentUserRoom){
+          allSockets[i].socket.send(parsedMessage.payload.message)
+        }
+      }
+    }
+  })
+})
